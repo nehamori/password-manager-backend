e47bd2f2bd4132ce78abe40a486d9ca1fe39cd6f
nehamori <nikita-kuzmichenko@mail.ru>
2026-03-08 22:46:15 +0600

    add alembic migrations

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -2,5 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     DATABASE_URL: str
     TELEGRAM_BOT_TOKEN: str
