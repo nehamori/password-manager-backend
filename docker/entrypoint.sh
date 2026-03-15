6cf0c1423d862a5d7fe1ea765ab780561e43bd18
nehamori <nikita-kuzmichenko@mail.ru>
2026-03-15 19:20:43 +0600

    alpine

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,13 @@
 set -e
 
 echo "Injecting Doppler secrets into environment variables..."
-source <(doppler secrets download --no-file --format env)
+DOPPLER_ENV_FILE="$(mktemp)"
+trap 'rm -f "$DOPPLER_ENV_FILE"' EXIT INT TERM
+
+doppler secrets download --no-file --format env > "$DOPPLER_ENV_FILE"
+set -a
+. "$DOPPLER_ENV_FILE"
+set +a
 
 echo "Applying Alembic migrations..."
 alembic -c /app/alembic.ini upgrade head
